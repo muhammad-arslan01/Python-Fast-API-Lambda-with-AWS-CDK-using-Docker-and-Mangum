d1068c1855d57576b6ea210a8bcc969da24d4116
Farukh Saleem <muhammadarclan@gmail.com>
2025-03-27 01:48:52 -0700

    Adds env variables

--- cdk/lib/backend-admin-panel-stack.ts
+++ cdk/lib/backend-admin-panel-stack.ts
@@ -2,30 +2,43 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as path from 'path';
-import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
-import * as ecr from 'aws-cdk-lib/aws-ecr'; // Import the ECR module
+import * as ecr from 'aws-cdk-lib/aws-ecr';  
+import { ConfigService } from '@nestjs/config';
 
 export class BackendAdminPanelStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, private readonly configService?:ConfigService) {
     super(scope, id, props);
 
+      let environmentVariables;
+      if(configService){
+        environmentVariables = {
+          DATABASE_HOST:configService.get('DATABASE_HOST'), 
+          DATABASE_PORT: configService.get('DATABASE_PORT'),
+          DATABASE_USER: configService.get('DATABASE_USER'),
+          DATABASE_PASSWORD: configService.get('DATABASE_PASSWORD'),
+          DATABASE_NAME: configService.get('DATABASE_NAME'),
+          CLIENT_ID:configService.get('CLIENT_ID'),
+          CLIENT_SECRET:configService.get('CLIENT_SECRET'),
+          NODE_ENV:configService.get('NODE_ENV')
+        };
+        
+      }
 
        // Reference the existing ECR repository
        const repository = ecr.Repository.fromRepositoryAttributes(this, 'BackendAdminPanelRepository', {
-        repositoryName: 'admin-panel-python-repo', // Replace with your ECR repository name
+        repositoryName: 'admin-panel-python-repo',  
         repositoryArn: `arn:aws:ecr:us-east-1:722365352638:repository/admin-panel-python-repo`,
       });
 
       const fastApiLambda = new lambda.DockerImageFunction(this, 'BackendAdminPanelLambda', {
         code: lambda.DockerImageCode.fromEcr(repository, {
-          tagOrDigest: 'latest', // Use a specific tag or digest if required
+          tagOrDigest: 'latest',  
         }),
-      memorySize: 512,   // Increase memory if needed
+      memorySize: 512,    
       timeout: cdk.Duration.seconds(30),
     });
 
-    // Define an API Gateway and connect it to the Lambda function
+     
     new apigateway.LambdaRestApi(this, 'BackendAdminPanelAPI', {
       handler: fastApiLambda,
       proxy: true,
